bcaba9efc5e76f96d0067aaafcbf028f246ae9fe
Wesley Meiring <wes.meiring@gmail.com>
2021-03-11 10:03:48 +0100

    Added data files and code for data visualising

---
+++ pie_graphs.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+###Create a pie graph for monthly expenses
+
+# Imports txt File
+cost = np.loadtxt("graph_data\costs_too.txt", dtype='str', delimiter=',')
+# Puts float data in array
+costs = cost[1:, 2:].astype("float")
+# Puts names of months which are strings in arrays
+month_names = cost[1:, 1].astype("str")
+# Sums up the total of each months expenses
+total_monthly_cost = np.sum(costs, axis=1)
+
+# Size of pie graph
+fig = plt.figure(figsize=(10, 10))
+# Figures that need to go into pie graph
+plt.pie(total_monthly_cost, labels=month_names, autopct='%1.1f%%')
+plt.title("Break down monthly total expenditure")
+
+plt.show
+
+category_names = cost[0, 2:].astype("str")
+total_category_cost = np.sum(costs, axis=0)
+
+fig = plt.figure(figsize=(10, 10))
+# Figures that need to go into pie graph
+plt.pie(total_category_cost, labels=category_names, autopct='%1.1f%%')
+plt.title("Break down total expenditure per category")
+
+plt.show()
---
+++ scatterplot_graph.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# Imports csv file
+galaxy = np.genfromtxt('graph_data\InaGalaxyFarAway.csv',dtype="str", delimiter=',') #import InaGalaxyFarAway file
+dic={"Fantasia":"r", "Utopia": "b", "Barsoom":"y", "Westeros":"g"}         # assigns colours to each continent
+
+n= galaxy[1:,1].astype("float")            # pulls data of population
+area =n**0.25                              # determines size of bubble
+
+x = galaxy[1:,2].astype("float")           # pulls data of GDP per capita
+y = galaxy[1:,3].astype("float")           # pulls data of life expectancy
+continents=galaxy[1:,0]                    # pulls name of continents
+colours=[]
+for c in continents:                       # appends colours to each contiennt as it loops through list
+    colours.append(dic[c])
+
+
+# Designs Scatter plot graph
+plt.scatter(x, y, s=area, c=colours, cmap="jet")
+plt.title("Scatter plot representing GDP vs life expentancy and includes population size")
+plt.xlabel("GDP per capita")
+plt.ylabel("Life expectancy")
+
+plt.show()
